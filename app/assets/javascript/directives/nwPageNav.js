a1f6338ae3740cac7be2b6fd26d1b22721eb0ba6
lemonflip <me@lisa-lemons.com>
2017-01-25 11:13:41 -0500

    added category filtering and nav

---
+++ app/assets/javascript/directives/nwPageNav.js
@@ -0,0 +1,14 @@
+angular.module("NoteWrangler").directive("nwPageNav", function(){
+    return {
+        replace: true,
+        restrict: "E",
+        templateUrl: "assets/templates/directives/nwPageNav.html",
+        controller: function($scope, $location){
+            $scope.isPage = function(name) {
+                // /\/notes($|\/)/
+                
+                return new RegExp("/" + name + "($|/)").test($location.path());
+            };
+        }
+    };
+});
